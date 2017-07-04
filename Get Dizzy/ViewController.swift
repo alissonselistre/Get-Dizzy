606370d35813f7c40c18f8abf9c6e62426ce9a51
Alisson Selistre <alissonselistre@gmail.com>
2017-07-04 13:27:12 -0300

    Initial Commit

---
+++ Get Dizzy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Get Dizzy
+//
+//  Created by Alisson L. Selistre on 04/07/17.
+//  Copyright © 2017 Alisson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
