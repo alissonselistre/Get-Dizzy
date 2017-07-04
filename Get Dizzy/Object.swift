3b1de26df4b2924d8c25b6808b169f39a87225bc
Alisson Selistre <alissonselistre@gmail.com>
2017-07-04 14:36:59 -0300

    - Created a game template where a vase is added randomly in a place near of the player. - Removed the unused assets.

---
+++ Get Dizzy/Object.swift
@@ -0,0 +1,24 @@
+//
+//  Object.swift
+//  Get Dizzy
+//
+//  Created by Alisson L. Selistre on 04/07/17.
+//  Copyright © 2017 Alisson. All rights reserved.
+//
+
+import ARKit
+
+class Object: SCNNode {
+    
+    func loadModel() {
+        guard let virtualObjectScene = SCNScene(named: "Models.scnassets/vase/vase.scn") else { return }
+        
+        let wrapperNode = SCNNode()
+        
+        for child in virtualObjectScene.rootNode.childNodes {
+            wrapperNode.addChildNode(child)
+        }
+        
+        self.addChildNode(wrapperNode)
+    }
+}
--- Get Dizzy/ViewController.swift
+++ Get Dizzy/ViewController.swift
@@ -6,20 +6,42 @@
 //  Copyright © 2017 Alisson. All rights reserved.
 //
 
-import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let scene = SCNScene()
+        sceneView.scene = scene
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let configuration = ARWorldTrackingSessionConfiguration()
+        sceneView.session.run(configuration)
+        
+        addObject()
     }
-
-
+    
+    func addObject() {
+        let object = Object()
+        object.loadModel()
+        
+        let xPos = randomPosition(lowerBound: -1.5, upperBound: 1.5)
+        let yPos = randomPosition(lowerBound: -1.5, upperBound: 1.5)
+        
+        object.position = SCNVector3(xPos, yPos, -1)
+        
+        sceneView.scene.rootNode.addChildNode(object)
+    }
+    
+    func randomPosition (lowerBound lower: Float, upperBound upper: Float) -> Float {
+        return Float(arc4random()) / Float(UInt32.max) * (lower - upper) + upper
+    }
 }
 
